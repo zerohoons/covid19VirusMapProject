0c2faaaf1d0a7be824597a204f6e6f9146fb7004
zerohoons <zerohoons@gmail.com>
2021-07-30 18:11:38 +0900

    wleh ㅙㅍㄷㄱ click ㅓznjfl dhksfy vktldgkrl

---
+++ Covid19MapSite/src/main/webapp/resources/js/map.js
@@ -0,0 +1,370 @@
+$(document).ready(function() {
+	$('#path-0').on({
+		click : function() {
+			$('path').not('#path-0').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-0').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-0').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-0').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-1').on({
+		click : function() {
+			$('path').not('#path-1').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-1').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-1').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-1').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-2').on({
+		click : function() {
+			$('path').not('#path-2').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-2').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-2').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-2').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-3').on({
+		click : function() {
+			$('path').not('#path-3').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-3').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-3').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-3').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-4').on({
+		click : function() {
+			$('path').not('#path-4').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-4').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-4').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-4').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-5').on({
+		click : function() {
+			$('path').not('#path-5').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-5').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-5').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-5').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-6').on({
+		click : function() {
+			$('path').not('#path-6').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-6').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-6').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-6').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-7').on({
+		click : function() {
+			$('path').not('#path-7').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-7').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-7').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-7').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-8').on({
+		click : function() {
+			$('path').not('#path-8').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-8').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-8').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-8').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-9').on({
+		click : function() {
+			$('path').not('#path-9').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-9').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-9').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-9').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-10').on({
+		click : function() {
+			$('path').not('#path-10').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-10').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-10').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-10').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-11').on({
+		click : function() {
+			$('path').not('#path-11').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-11').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-11').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-11').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-12').on({
+		click : function() {
+			$('path').not('#path-12').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-12').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-12').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-12').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-13').on({
+		click : function() {
+			$('path').not('#path-13').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-13').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-13').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-13').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-14').on({
+		click : function() {
+			$('path').not('#path-14').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-14').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-14').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-14').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+	$('#path-15').on({
+		click : function() {
+			$('path').not('#path-15').css("fill", "rgb(68, 67, 67)");
+			$(this).off('mouseleave');
+			$('path').not(this).on('mouseleave');
+			$('#path-15').css({
+				"fill" : "crimson",
+				"transition" : "0.8s"
+			})
+		},
+		mouseover : function() {
+			$('#path-15').css({
+				"fill" : "crimson",
+				"transition" : "1.5s"
+			});
+		},
+		mouseleave : function() {
+			$('#path-15').css({
+				"fill" : "rgb(68, 67, 67)",
+				"transition" : "1.5s"
+			})
+		}
+	});
+})
